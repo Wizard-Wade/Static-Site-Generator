8fd1cfccdcd739486349bff8cff40aff8e7d93da
Wizard-Wade <wade.vollink@gmail.com>
2025-04-15 22:44:03 -0600

    add block type validation to block markdown

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = '""'
+    HEADING = "#"
+    CODE = "```"
+    QUOTE = ">"
+    UNORDERED_LIST = "-"
+    ORDERED_LIST = "*. "
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered = []
@@ -6,3 +16,21 @@ def markdown_to_blocks(markdown):
         if block:
             filtered.append(block)
     return filtered
+
+def block_to_block_type(block):
+    first_word, *mid, last_word = block.split()
+    if first_word.startswith(BlockType.HEADING.value):
+        return BlockType.HEADING
+    if first_word.startswith(BlockType.CODE.value) and last_word.endswith(BlockType.CODE.value):
+        return BlockType.CODE
+    split_lines =  block.split("\n")
+    if all(list(map(lambda x: x.startswith(BlockType.QUOTE.value), split_lines))):
+        return BlockType.QUOTE 
+    if all(list(map(lambda x: x.startswith(BlockType.UNORDERED_LIST.value), split_lines))):
+        return BlockType.UNORDERED_LIST
+    for i in range(len(split_lines)):
+        if not split_lines[i].startswith(f"{i+1}. "):
+            break
+        if i == len(split_lines) -1:
+            return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -4,7 +4,7 @@ from block_markdown import *
 
 class TestblockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
-        md = """This is **bolded** paragraph
+        md = """# This is **bolded** paragraph
 
 This is another paragraph with _italic_ text and `code` here
 This is the same paragraph on a new line
@@ -15,34 +15,38 @@ This is the same paragraph on a new line
         self .assertEqual(
             blocks,
             [
-                "This is **bolded** paragraph",
+                "# This is **bolded** paragraph",
                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                 "- This is a list\n- with items",
             ],
         )
+        types = list(map(lambda block: block_to_block_type(block), blocks))
+        self.assertEqual(types, [BlockType.HEADING, BlockType.PARAGRAPH, BlockType.UNORDERED_LIST])
 
     def test_markdown_to_blocks_newlines(self):
         md = """
-This is **bolded** paragraph
+## This is **bolded** paragraph
 
 
 
 
-This is another paragraph with _italic_ text and `code` here
-This is the same paragraph on a new line
+```This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line```
 
-- This is a list
-- with items
+1. This is a list
+2. with items
 """
         blocks = markdown_to_blocks(md)
         self.assertEqual(
             blocks,
             [
-                "This is **bolded** paragraph",
-                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
-                "- This is a list\n- with items",
+                "## This is **bolded** paragraph",
+                "```This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line```",
+                "1. This is a list\n2. with items",
             ],
         )
+        types = list(map(lambda block: block_to_block_type(block), blocks))
+        self.assertEqual(types, [BlockType.HEADING, BlockType.CODE, BlockType.ORDERED_LIST])
 
 if __name__ == "__main__":
     unittest.main()
