bbb8c41a912dd5559dec478ac2bdfdbc6b5739e9
Wade Vollink <wvollink@ryancompanies.com>
2025-05-06 22:48:14 -0600

    add delete dir files command call it recursively from the delete public contents to empy out all folders and files

--- src/copy_static_to_public.py
+++ src/copy_static_to_public.py
@@ -5,14 +5,23 @@ import os
 #recommended to log the path of each file copied so we can see what's happening as we run and debug.
 
 def delete_public_contents():
-    public_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),"public")
+    public_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),"..//public")
     if not os.path.exists(public_path):
         os.mkdir(public_path)
         return
     
+    delete_dir_files_(public_path)
     
-    for dir in os.listdir(public_path):
-        os.remove
+def delete_dir_files_(dir_path):       
+    for name in os.listdir(dir_path):
+        fullpath = os.path.join(dir_path, name)
+        if os.path.isfile(fullpath):
+            os.remove(fullpath)
+        else:
+            delete_dir_files_(fullpath)
+            os.removedirs(fullpath)
+
+        
     
 
         
