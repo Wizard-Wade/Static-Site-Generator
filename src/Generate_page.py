85b0730b7a79486856137f29c4e52f314d6489bc
Wizard-Wade <wade.vollink@gmail.com>
2025-05-18 21:09:29 -0600

    recursively generate pages in content folder.

--- src/Generate_page.py
+++ src/Generate_page.py
@@ -1,22 +1,56 @@
 import markdown_to_html
 import shutil
 import os
+import pathlib
 
-def generate_page(from_path, template_path, dest_path):
-    if not os.path.exists(from_path):
-        from_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), from_path)
-        if not os.path.exists(from_path):
-            raise Exception("Could not find markdown content")
+def generate_website(dir_path_content, template_path, dest_dir_path):
+    if not os.path.exists(dir_path_content):
+        dir_path_content = os.path.join(os.path.dirname(os.path.realpath(__file__)), dir_path_content)
+        if not os.path.exists(dir_path_content):
+            raise Exception("Could not find content folder")
     
+    if not os.path.exists(dest_dir_path):
+        dest_dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), dest_dir_path)
+        if not os.path.exists(dest_dir_path):
+            raise Exception("Destination folder does not exist")
+        
     if not os.path.exists(template_path):
         template_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), template_path)
         if not os.path.exists(template_path):
             raise Exception("Could not find template")
     
+    generate_pages_recursive(dir_path_content, template_path, dest_dir_path)
+    
+    
+def generate_pages_recursive(dir_path, template_path, dest_path):
+    if not os.path.exists(dest_path):
+        os.mkdir(dest_path)
+
+    for name in os.listdir(dir_path):
+        s_fullpath = os.path.join(dir_path, name)
+        d_fullpath = os.path.join(dest_path, name)
+        if os.path.isfile(s_fullpath):
+            generate_page(s_fullpath, template_path,  pathlib.Path(d_fullpath).with_suffix(".html"))
+        else:
+            os.mkdir(d_fullpath)
+            generate_pages_recursive(s_fullpath, template_path, d_fullpath)
+
+def generate_page(from_path, template_path, dest_path):
+
+    if not os.path.exists(from_path):
+        from_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), from_path)
+        if not os.path.exists(from_path):
+            raise Exception("Could not find content folder")
+    
     if not os.path.exists(os.path.dirname(dest_path)):
         dest_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), dest_path)
         if not os.path.exists(os.path.dirname(dest_path)):
             raise Exception("Destination folder does not exist")
+        
+    if not os.path.exists(template_path):
+        template_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), template_path)
+        if not os.path.exists(template_path):
+            raise Exception("Could not find template")
 
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from textnode import *
 from copy_static_to_public import generate_public_assets
-from Generate_page import generate_page
+from Generate_page import generate_website, generate_page
 
 def main():
     generate_public_assets()
-    generate_page("..//content//index.md", "..//templates//template.html", "..//public//index.html")
+    #generate_page("..//content//index.md", "..//templates//template.html", "..//public//index.html")
+    generate_website("..//content", "..//templates//template.html", "..//public")
 
 main()
