745967d659a7c945f463d6489f68353898f69c9b
Wizard-Wade <wade.vollink@gmail.com>
2025-05-19 09:34:17 -0600

    adjust link references

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from Generate_page import generate_website, generate_page
 import sys
 
 def main():
-    basepath = "\\" + sys.argv[1] if sys.argv[1] is not None else "/"
+    basepath = sys.argv[1] if len(sys.argv)> 1 else "/"
     generate_assets("docs")
     generate_website(f"..//content", f"..//templates//template.html", f"..//docs", basepath)
 
